ddd32ec7aaa5affeb468c124b2817d82f81d31df
Sourabh Verma <sourabhvermasenior987@gmail.com>
2021-03-29 14:34:15 +0530

    front page version 1

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -43,19 +39,168 @@ class _HomePageState extends State<HomePage> {
               child: Container(
                 width: double.infinity,
                 decoration: BoxDecoration(
-                  color: Colors.white,
+                  color: Colors.grey[50],
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(40.0),
                     topRight: Radius.circular(40.0),
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [Text('All Rooms')],
+                child: Container(
+                  margin: EdgeInsets.all(32),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'All Rooms',
+                        style: TextStyle(
+                            fontSize: 28,
+                            color: Colors.indigo[900],
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Row(
+                        children: [
+                          RoomCard(
+                            iconPath: 'assets/bed.png',
+                            roomName: 'Bed room',
+                            noL: '4 Lights',
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          RoomCard(
+                            iconPath: 'assets/room.png',
+                            roomName: 'Living room',
+                            noL: '2 Lights',
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          RoomCard(
+                            iconPath: 'assets/kitchen.png',
+                            roomName: 'Kitchen',
+                            noL: '5 Lights',
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          RoomCard(
+                            iconPath: 'assets/bathtube.png',
+                            roomName: 'Bathroom',
+                            noL: '1 Lights',
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          RoomCard(
+                            iconPath: 'assets/house.png',
+                            roomName: 'Outdoor',
+                            noL: '5 Lights',
+                          ),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          RoomCard(
+                            iconPath: 'assets/balcony.png',
+                            roomName: 'Balcony',
+                            noL: '2 Lights',
+                          )
+                        ],
+                      )
+                    ],
+                  ),
                 ),
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    offset: const Offset(
+                      5.0,
+                      5.0,
+                    ),
+                    blurRadius: 6),
+              ]),
+              height: 60,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    'assets/bulb.png',
+                    scale: 3,
+                  ),
+                  Image.asset(
+                    'assets/home.png',
+                    scale: 3,
+                  ),
+                  Image.asset(
+                    'assets/settings.png',
+                    scale: 3,
+                  )
+                ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class RoomCard extends StatelessWidget {
+  RoomCard({this.iconPath, this.roomName, this.noL});
+  final String iconPath;
+  final String roomName;
+  final String noL;
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Card(
+        elevation: 2,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  iconPath,
+                  scale: 3.5,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  roomName,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  noL,
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.yellow[600],
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
